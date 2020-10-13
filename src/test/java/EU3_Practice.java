abbb7f6ed6656ba4c3332e045be1ecd7181a1f5f
EU3 Batch GitHup <msen2020@hotmail.com>
2020-10-14 01:12:52 +0200

    Committed EU3 Practice opening with  A pull request

--- src/test/java/EU3_Practice.java
+++ src/test/java/EU3_Practice.java
@@ -1,7 +1,7 @@
 public class EU3_Practice {
 
     public static void main(String[] args) {
-        System.out.println("EU3 is opening pull request");
+        System.out.println("EU3 is opening a pull request");
 
         // commit your change
         // with a nice message
